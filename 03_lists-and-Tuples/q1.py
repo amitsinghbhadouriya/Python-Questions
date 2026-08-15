2ae90368d0c5278001eb5a888f62daf8e367cc05
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-16 01:43:54 +0530

    add f3 q1

---
+++ 03_lists-and-Tuples/q1.py
@@ -0,0 +1,12 @@
+# Write a program to store seven fruits in a list entered by the user.
+
+a = input("Enter fruit1: ")
+b = input("Enter fruit2: ")
+c = input("Enter fruit3: ")
+d = input("Enter fruit4: ")
+e = input("Enter fruit5: ")
+f = input("Enter fruit6: ")
+g = input("Enter fruit7: ")
+
+Fruits = [a, b, c, d, e, f, g]
+print(Fruits)
