8308e95ed4fb552df414b1492be141cba49f3e97
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-15 20:19:00 +0530

    add f2 q2

--- 02_Strings/q2.py
+++ 02_Strings/q2.py
@@ -1,5 +1,9 @@
-# Write a program to detect double space in a string
+# Write a program to detect double space in a string and replace it with single space
 
 Message = "Hey  How  are  you  what  you  learn  about  python"
 detect_double_space = Message.find("  ")
+print(Message)
 print(detect_double_space)
+
+Single_space = Message.replace("  ", " ")
+print(Single_space)
