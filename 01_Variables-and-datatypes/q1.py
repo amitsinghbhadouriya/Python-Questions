1205ac0939e1672e57a2c4471b9946c5e3ff277d
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-15 19:53:35 +0530

    add f1 q1

---
+++ 01_Variables-and-datatypes/q1.py
@@ -0,0 +1,7 @@
+# Assign a message to a variable, and then print that message. Then change the value of the variable to a new message, and print the new message.
+
+a = "hey"
+print(a)
+
+a = "Hello world"
+print(a)
