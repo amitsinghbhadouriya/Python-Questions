822988d6d10bc310398100d63e431270902afe85
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-15 20:16:20 +0530

    add f2 q2

---
+++ 02_Strings/q2.py
@@ -0,0 +1,5 @@
+# Write a program to detect double space in a string
+
+Message = "Hey  How  are  you  what  you  learn  about  python"
+detect_double_space = Message.find("  ")
+print(detect_double_space)
