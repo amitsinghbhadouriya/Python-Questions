3797af96b07f78dc1aff425e955aebf86a87965f
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-15 20:40:23 +0530

    add f1 q7

---
+++ 01_Variables-and-datatypes/q7.py
@@ -0,0 +1,6 @@
+# Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print() calls to see the results.
+
+print(5+3)
+print(9-1)
+print(2*4)
+print(16/2)
