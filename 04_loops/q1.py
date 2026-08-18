6e454703667b3e743509fa6029561228c70f201d
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-18 23:37:38 +0530

    add f4 q1

---
+++ 04_loops/q1.py
@@ -0,0 +1,9 @@
+# Write a program to print multiplication table of a given number using for loop.
+
+n = int(input("Enter a number: "))
+i = 1
+
+while(i<=10):
+    print(f"{n} x {i} = {n*i}")
+    i += 1
+    
