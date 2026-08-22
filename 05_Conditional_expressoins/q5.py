b1fb4d558b62fde7ccd5e0699b59300c68bf60b2
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-22 18:39:32 +0530

    add f5 q5

---
+++ 05_Conditional_expressoins/q5.py
@@ -0,0 +1,7 @@
+# Write a program to find out whether a given post is talking about "Amit" or not.
+
+post = input("Enter a post: ")
+if "Amit" in post:
+    print("This post is related to Amit")
+else:
+    print("This post is not related to Amit")
