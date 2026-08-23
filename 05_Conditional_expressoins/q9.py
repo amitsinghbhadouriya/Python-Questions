9b7992e948a59494e43a75a5206126dd63621223
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-23 20:08:18 +0530

    add f5 q9

---
+++ 05_Conditional_expressoins/q9.py
@@ -0,0 +1,7 @@
+# Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow' or 'red'. 
+# Write an if statement to test whether the alien's color is green. if it is, print a message that the player just earned 5 points.
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You earned 5 points")
