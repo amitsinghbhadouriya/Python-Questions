72d007380a55705afb3aa5a25806e940db91c9e3
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-23 20:14:41 +0530

    update f5 q9

--- 05_Conditional_expressoins/q9.py
+++ 05_Conditional_expressoins/q9.py
@@ -1,7 +1,12 @@
 # Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow' or 'red'. 
-# Write an if statement to test whether the alien's color is green. if it is, print a message that the player just earned 5 points.
-
-alien_color = 'green'
+# If the alien color is green, print a message that the player just earned 5 points for shooting the alien.
+# if the alien color is yellow, print a message that the player just earned 10 points.
+# if the alien color is red, print a message that the player just earned 15 points.
+alien_color = input("Enter a color, (must be 'green', 'yellow', 'red'): ")
 
 if alien_color == 'green':
     print("You earned 5 points")
+elif alien_color == 'yellow':
+    print("You earned 10 points")
+else:
+    print("You earned 15 points")
