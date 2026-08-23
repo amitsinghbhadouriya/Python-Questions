ee5598a64b9464b2180e36cbb7dd4da73ee59a44
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-23 20:33:10 +0530

    add f5 q11

---
+++ 05_Conditional_expressoins/q11.py
@@ -0,0 +1,19 @@
+# Make a list of your three favorite fruits and call it favorite_fruits.
+# Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list,the if block should print a statement, such as You really like bananas!
+
+favorite_fruits = ['mango','apple','kiwi']
+
+if 'mango' in favorite_fruits:
+    print("I like mangoes")
+    
+if 'apple' in favorite_fruits:
+    print("I like apples")
+
+if 'kiwi' in favorite_fruits:
+    print("I like kiwi")
+
+if 'orange' in favorite_fruits:
+    print("I like oranges")
+    
+if 'banana' in favorite_fruits:
+    print("I like bananas")
