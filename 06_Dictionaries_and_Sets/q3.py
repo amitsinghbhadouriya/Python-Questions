6298a7243c505c4cbc135559b0c7b1c21b9ab93f
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-26 18:23:07 +0530

    add f6 q3

--- 06_Dictionaries_and_Sets/q3.py
+++ 06_Dictionaries_and_Sets/q3.py
@@ -1,3 +1,11 @@
+# Can we have a set with 18(int) and "18"(str) as a values in it?
+
+values = {18, "18"}
+print(values)
+print(type(values))
+for value in values:
+    print(type(value))
+
 # What will be the length of following set s:
 
 s = set()
