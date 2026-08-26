1b3284859c4f2ddeff228b1e6803f90199d32a0d
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-27 01:59:46 +0530

    add f6 q7

---
+++ 06_Dictionaries_and_Sets/q7.py
@@ -0,0 +1,19 @@
+# Make a dictionary containing three major rivers and the country each river runs through. One key-value pair might be 'nile': 'egypt'.
+# Use a loop to print a sentence about each river, such as The Nile runs through Egypt.
+# Use a loop to print the name of each river included in the dictionary.
+# Use a loop to print the name of each country included in the dictionary.
+
+rivers = {
+    'nile': 'egypt',
+    'amazon': 'brazil',
+    'ganges': 'india',
+}
+
+for key, value in rivers.items():
+    print(f"The {key.title()} runs through {value.title()}")
+    
+for river in rivers.keys():
+    print(river)
+    
+for countries in rivers.values():
+    print(countries)
