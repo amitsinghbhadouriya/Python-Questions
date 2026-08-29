3376c73d5690fff87b7259839cb0e4ec615a6a86
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-29 23:42:20 +0530

    add f7 q4

---
+++ 07_Functions_and_Recursion/q4.py
@@ -0,0 +1,14 @@
+# Write a python function to print first n lines of the following pattern for n = 3
+'''
+***
+**
+*
+'''
+
+def pattern(n):
+    for i in range(n, 0, -1):
+        print("*" * i)
+
+
+n = int(input("Enter a value: "))
+pattern(n)
