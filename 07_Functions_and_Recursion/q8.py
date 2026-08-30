21aeb6fffe1d04095acf9dab29ee3be7cca0bf77
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-30 13:50:52 +0530

    add f7 q8

---
+++ 07_Functions_and_Recursion/q8.py
@@ -0,0 +1,6 @@
+# Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter. call the function, and make sure the message displays correctly.
+
+def display_message():
+    return "I am learning about python function in this chapter."
+
+print(display_message())
