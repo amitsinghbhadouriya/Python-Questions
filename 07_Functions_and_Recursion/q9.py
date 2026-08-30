5bfa366f388cf3120bb36a5ae885d7a3615ea9ab
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-30 13:51:14 +0530

    add f7 q9

---
+++ 07_Functions_and_Recursion/q9.py
@@ -0,0 +1,6 @@
+# Write a function called favorite_book() that accepts one parameter, title. The function should print a message, such as one of my favorite books is alice in wonderland. call the function, make sure to include a book title as an argument in the function call.
+
+def favorite_book(title):
+    print(f"My favorite book is {title}")
+
+favorite_book("Atomic habits")
