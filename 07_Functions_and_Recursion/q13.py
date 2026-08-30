69b8a22d7173988e000a2e9488617d7be2636cad
Amit Singh <abhaybhadouriya88@gmail.com>
2026-08-30 20:30:59 +0530

    add f7 q13

---
+++ 07_Functions_and_Recursion/q13.py
@@ -0,0 +1,15 @@
+# Write a function called make_album() that builds a dictionary describing a music album. The function should take in an artist name and an album title, and it should return a dictionary containing these two pieces of information. Use the function to make three dictionaries representing different albums. Print each return value to show that the dictionaries are storing the album information correctly.
+# Use none to add an optional parameter to make_album() that allows you to store the number os songs on an album. If the calling line includes a value for the number of songs, add that value to the album's dictionary. Make at least one new function call that includes that number of songs on an album.
+
+def make_album(album_name, album_title, no_of_songs=None):
+    album = {
+        'album_name': album_name,
+        'album_title': album_title,
+    }
+    if no_of_songs:
+        album['Number_of_Songs'] = no_of_songs
+    return album
+
+print(make_album('Thriller','Michael Jackson'))
+print(make_album('Scorpion','Drake'))
+print(make_album('After Hours','The Weeknd', 5))
